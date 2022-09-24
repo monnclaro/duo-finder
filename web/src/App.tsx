8c6b5da63205399fea99d235eb040a1fb6f517a8
Augusto Monclaro <monclaro.a@gmail.com>
2022-09-24 12:24:16 -0300

    All files

---
+++ web/src/App.tsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import "./styles/main.css";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import LogoImage from "./assets/logo-nlw-esports.svg";
+
+import { useEffect, useState } from "react";
+import { GameBanner } from "./components/GameBanner";
+import { CreateAdModal } from "./components/CreateAdModal";
+import { CreateAdBanner } from "./components/CreateAdBanner";
+
+interface Game {
+  id: string;
+  title: string;
+  bannerURL: string;
+  _count: {
+    ads: number;
+  };
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    axios("http://localhost:3000/games").then((response) => {
+      setGames(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 select-none">
+      <img src={LogoImage} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerURL}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
+      </div>
+
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <CreateAdModal />
+      </Dialog.Root>
+    </div>
+  );
+}
+
+export default App;
